f3a631ecebf9a3c3f57808ef09aad0a1a6a18b32
Dr. Mary Lebens <52386621+marylebens@users.noreply.github.com>
2024-05-26 15:43:20 +0000

    Initial Commit

---
+++ MilesPerGallonCalculator/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+* Miles Per gallon Calculator
+* Dr. Mary Lebens
+* Last Updated 5/26/2024
+
+* Calculates the miles per gallon based on the number of 
+* miles driven and the number of gallons of gas used. 
+* Prompts the user for the values.
+*/
+
+Console.Write("Enter the number of miles driven: ");
+int milesDriven = int.Parse(Console.ReadLine());
+Console.Write("Enter the number of gallons of gas used: ");
+int gallonsUsed = int.Parse(Console.ReadLine());
+double mpg = milesDriven / gallonsUsed;
+Console.Write("Ths miles per gallon (MPG) is: " + mpg);
